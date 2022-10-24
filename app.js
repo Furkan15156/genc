c89d71c8c8286684590bae8027ce6fd53a91bc6e
Furkan Nazlı <Furkan@Sena-MacBook-Air.local>
2022-10-24 23:32:57 +0300

    Revert "suleyman branchi"
    This reverts commit d902440131d0be423c4ec28b334064c3bebe2b9e.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-console.log('okay genc')
+console.log('okay')
 
 mongoose.connect('mongodb://localhost:27017/todolistDB');
 
